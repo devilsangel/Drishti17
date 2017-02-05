341d84c045ff063b89afe72911cdff468f785045
Nirmal <ksnirm@gmail.com>
2017-02-05 20:46:42 +0530

    event page more

--- app/src/main/java/com/drishti/drishti17/ui/EventDetail.java
+++ app/src/main/java/com/drishti/drishti17/ui/EventDetail.java
@@ -3,9 +3,11 @@ package com.drishti.drishti17.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.drishti.drishti17.R;
 import com.drishti.drishti17.ui.factory.Blur.Blur;
+import com.drishti.drishti17.util.Import;
 
 public class EventDetail extends AppCompatActivity {
 
@@ -19,6 +21,11 @@ public class EventDetail extends AppCompatActivity {
 
     private void setUpUI() {
         new Blur((ImageView) findViewById(R.id.back), this, 3, 5).applyBlur(true, findViewById(R.id.inner_layout));
+
+        Import.settypefaces(this, "SourceSansPro-Regular.otf", (TextView) findViewById(R.id.desp));
+        Import.settypefaces(this, "SourceSansPro-Regular.otf", (TextView) findViewById(R.id.time));
+        Import.settypefaces(this, "SourceSansPro-Regular.otf", (TextView) findViewById(R.id.date));
+        Import.settypefaces(this, "SourceSansPro-Regular.otf", (TextView) findViewById(R.id.location));
 
     }
 
